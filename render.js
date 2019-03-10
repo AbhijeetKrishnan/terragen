86e5d6f3ced64430538598fbef21311e630dcde1
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2019-03-10 15:56:52 -0400

    Add transform to Perlin grid while calculating noise

--- render.js
+++ render.js
@@ -3,10 +3,10 @@ function main() {
     var ctx = canvas.getContext('2d');
     var w = canvas.width;
     var h = canvas.height;
-    perlin = new Perlin(w, h);
+    perlin = new Perlin(30, 40);
     for (var i = 0; i < h; i++) {
         for (var j = 0; j < w; j++) {
-            var noise = 255 * perlin.perlin2(j + 0.5, i + 0.5);
+            var noise = 255 * perlin.perlin2(j + 0.5, i + 0.5, w, h);
             //console.log(noise);
             ctx.fillStyle = "rgb(" + noise + "," + noise + "," + noise + ")";
             ctx.fillRect(j, i, 1, 1);
