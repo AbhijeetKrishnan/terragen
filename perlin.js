8bc18272fb5304239b90db31ff8bf3c5e3a7333a
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2019-03-10 17:41:13 -0400

    Add frequencies and amplitudes

--- perlin.js
+++ perlin.js
@@ -2,23 +2,6 @@
  * Class to generate Perlin noise
  */
 
-// Hash lookup table as defined by Ken Perlin.  
-// This is a randomly arranged array of all numbers from 0-255 inclusive.
-const permutation = [151, 160, 137, 91, 90, 15, 
-    131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 
-    190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
-    88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166,
-    77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
-    102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196,
-    135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
-    5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
-    223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
-    129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228,
-    251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
-    49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
-    138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
-];
-
 class Perlin {
     constructor(ixmax, iymax) {
         // size of Perlin grid
@@ -53,6 +36,7 @@ class Perlin {
     computeGradient() {
         for (var y = 0; y < this.IYMAX; y++) {
             for (var x = 0; x < this.IXMAX; x++) {
+                // transform to [-1, 1]
                 this.Gradient[y][x][0] = 2 * this.rng() - 1;
                 this.Gradient[y][x][1] = 2 * this.rng() - 1;
 
@@ -109,13 +93,14 @@ class Perlin {
         var y1 = y0 + 1;
 
         // Determine interpolation weights
-        // Could also use higher order polynomial/s-curve here
         var sx = Perlin.fade(x - x0);
+        // var sx = x - x0;
         console.assert(sx >= 0.0 && sx <= 1.0, {
             "message": "sx is not in [0, 1]",
             "sx": sx
         });
         var sy = Perlin.fade(y - y0);
+        // var sy = y - y0;
         console.assert(sy >= 0.0 && sy <= 1.0, {
             "message": "sy is not in [0, 1]",
             "sy": sy
@@ -132,6 +117,7 @@ class Perlin {
         //console.log(ix0 + " " + ix1);
         value = Perlin.lerp(ix0, ix1, sy);
 
+        console.assert(value >= -1.0 && value <= 1.0, "Value in incorrect range");
         return value;
     }
 }
--- render.js
+++ render.js
@@ -3,12 +3,13 @@ function main() {
     var ctx = canvas.getContext('2d');
     var w = canvas.width;
     var h = canvas.height;
-    perlin = new Perlin(30, 40);
+    perlin = new Perlin(16, 16); // could possible vary this as a ratio of world size
     for (var i = 0; i < h; i++) {
         for (var j = 0; j < w; j++) {
-            var noise = 255 * perlin.perlin2(j + 0.5, i + 0.5, w, h);
+            var n = perlin.perlin2((j + 0.5) / 1, (i + 0.5) / 1, w, h) * 1;
+            var noise = 255 * (perlin.perlin2(j + 0.5, i + 0.5, w, h) / 2 + 0.5);
             //console.log(noise);
-            ctx.fillStyle = "rgb(" + noise + "," + noise + "," + noise + ")";
+            ctx.fillStyle = "rgb(" + 0 + "," + noise + "," + 0 + ")";
             ctx.fillRect(j, i, 1, 1);
         }
     }
